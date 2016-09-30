2d8583cdc07357cdcf68bcd47a3d4a3a31440928
Paul <git@wittig.dk>
2016-09-30 16:50:16 +0200

    1

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace StraightLine.Controllers
     public class ApiController : Controller
     {
         //
-        // POST: /Api/Git
+        // POST: /Api/GitPullUpdateServer
         [HttpPost]
         [AllowAnonymous]
         public IActionResult GitPullUpdateServer([FromBody]dynamic data)
@@ -19,6 +20,7 @@ namespace StraightLine.Controllers
             try
             {
                 var testJsonResult = JsonConvert.DeserializeObject(data.ToString());
+                Process.Start("/bin/sh", "/home/ubuntu/mvc-run/update.sh");
                 return Ok();
             }
             catch (Exception)
