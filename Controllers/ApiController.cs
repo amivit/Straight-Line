9fe3a067f837769dc176b66a2ac71e541a087d76
Paul <git@wittig.dk>
2016-09-30 19:26:40 +0200

    12

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -13,27 +13,19 @@ namespace StraightLine.Controllers
     {
         // This method is called when appveyor succesfully finishes a build on the master branch (check after_deploy in appveyor.yml)
         // It starts a script to update to the latest version that appveyor has already build and uploaded to S3 for us
-        // TODO: Encrypt the key in appveyor.yml
+        // TODO: Validate the call
         // POST: /Api/GitPullUpdateServer
         [HttpPost]
         [AllowAnonymous]
         public IActionResult GitPullUpdateServer([FromBody]dynamic data)
         {
-            try
-            {
-                var testJsonResult = JsonConvert.DeserializeObject(data.ToString());
-                ProcessStartInfo psi = new ProcessStartInfo();
-                psi.FileName = "/bin/bash";
-                psi.UseShellExecute = false;
-                psi.RedirectStandardOutput = false;
-                psi.Arguments = "-c /home/ubuntu/mvc-run/update.sh &";
-                Process.Start(psi);
-                return Ok();
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-            }
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "/bin/bash";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = false;
+            psi.Arguments = "\"-c /home/ubuntu/mvc-run/update.sh &\"";
+            Process.Start(psi);
+            return Ok();
         }
     }
 }
