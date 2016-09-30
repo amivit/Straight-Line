47443b0a52e236c869647ad817226f749bac77d8
Paul <git@wittig.dk>
2016-09-30 17:17:59 +0200

    4

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -11,7 +11,9 @@ namespace StraightLine.Controllers
 {
     public class ApiController : Controller
     {
-        //
+        // This method is called when appveyor succesfully finishes a build on the master branch (check after_deploy in appveyor.yml)
+        // It starts a script to update to the latest version that appveyor has already build and uploaded to S3 for us
+        // TODO: Encrypt the key in appveyor.yml
         // POST: /Api/GitPullUpdateServer
         [HttpPost]
         [AllowAnonymous]
@@ -20,7 +22,7 @@ namespace StraightLine.Controllers
             try
             {
                 var testJsonResult = JsonConvert.DeserializeObject(data.ToString());
-                Process.Start("/bin/sh", "/home/ubuntu/mvc-run/update.sh");
+                Process.Start("bash -c /home/ubuntu/mvc-run/update.sh");
                 return Ok();
             }
             catch (Exception)
