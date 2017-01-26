1cbf0c749867d1d9c235bb448c29847dff7a2ebb
Kasper Johnsen <kasperjohnsen@live.dk>
2017-01-26 12:22:08 +0100

    Lavet bræt

--- Models/Battleship/Board.cs
+++ Models/Battleship/Board.cs
@@ -7,30 +7,61 @@ namespace StraightLine.Models.Battleship
 {
     public class Board
     {
+        private Tile t;
+        private Ship s;
         public List<Tile> Tiles { get; set; }
+        public  List<Ship> Ships { get; set; } 
 
         public Board()
         {
             this.Tiles = new List<Tile>();
+            this.Ships = new List<Ship>();
             GenerateTiles();
+            GenerateShips();
         }
 
         private void GenerateTiles()
         {
-            Tile t = new Tile();
-            t.Hit = false;
-            t.Ship = false;
-            t.id = "a1";
-            t.x = 100;
-            t.y = 100;
-            Tiles.Add(t);
-            t = new Tile();
-            t.Hit = false;
-            t.Ship = false;
-            t.id = "a2";
-            t.x = 200;
-            t.y = 200;
-            Tiles.Add(t);
+            int teller = 0;
+            int x = 0;
+            int y = 50;
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    t = new Tile();
+                    t.Hit = false;
+                    t.Ship = false;
+                    t.id = teller.ToString();
+                    t.x = x + 50;
+                    x = t.x;
+                    t.y = y;
+                    Tiles.Add(t);
+                    teller = teller + 1;
+                }
+                y = y + 50;
+                x = 0;
+            }
+        }
+
+        private void GenerateShips()
+        {
+            s = new Ship();
+            s.id = 0.ToString();
+            s.lenght = 3;
+            Ships.Add(s);
+
+            s = new Ship();
+            s.id = 1.ToString();
+            s.lenght = 2;
+            Ships.Add(s);
+
+            s = new Ship();
+            s.id = 2.ToString();
+            s.lenght = 1;
+            Ships.Add(s);
+
+
         }
     }
 }
---
+++ Models/Battleship/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StraightLine.Models.Battleship
+{
+    public class Ship
+    {
+        public string id { get; set; }
+        public List<Tile> placement { get; set; }
+
+        public int lenght { get; set; }
+
+        public bool clicked { get; set; }
+        public string colour { get; set; }
+
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+    }
+}
