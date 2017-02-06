9a2d5dfd116605403bd685d72db02ef62f2bdda0
Paul <git@wittig.dk>
2017-02-06 11:52:07 +0100

    Working with Vue.js

--- Controllers/MonsterSlayerController.cs
+++ Controllers/MonsterSlayerController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StraightLine.Controllers
 {
-    public class MinecraftController : Controller
+    public class MonsterSlayerController : Controller
     {
         public IActionResult Index()
         {
